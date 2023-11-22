a0fa261647214f89b6b0f199a4376a29876ba6bb
ESTEVE Fatima <FEsteve@arfp.asso.fr>
2023-11-22 10:25:38 +0100

    mis à jour

---
+++ Algo/java/monpremierprojet/src/exexpressionbooleenne/App.java
@@ -0,0 +1,25 @@
+package exexpressionbooleenne;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 1;
+		int c = 7;
+		
+		
+			boolean test1 = (a < b);
+			boolean test2 = (a < b) && (a > c);
+			boolean test3 = (a < b) || (a > c);
+			System.out.println(test1);
+			System.out.println(test2);
+			System.out.println(test3);
+			
+			
+			
+			
+			
+			
+	}
+}
---
+++ Algo/java/monpremierprojet/src/heloworld/App.java
@@ -0,0 +1,35 @@
+package heloworld;
+
+public class App {
+
+	public static void main(String[] args) {
+		/*
+		 * Exemple
+		 * de
+		 * code
+		 * Java
+		 */
+		//Declaration d'une variable de type chaine de caractères	
+		
+		
+		// Declaration d'une variable de type entier
+		int nombre;
+		nombre = 5;
+		float nombre2;
+		nombre2 = 2.5f;
+		double nombre3;
+		nombre3 = 3.25;
+		long nombre4;
+		nombre4 = 12254;
+		
+		int resultat = 3+2;
+		//déclaratio d'une variable de type booleen
+		
+		String maVariable;
+		
+		
+			maVariable ="Hello World";
+		System.out.println("Hello World");
+	}
+
+}
